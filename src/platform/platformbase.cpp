26e738a523fbfe917ea53fe02a25b22eb6f85b93
Isaac Garzon <ncebdacr@duck.com>
2025-04-08 19:28:17 +0300

    platform: move base code to `platformbase.cpp` to avoid duplication
    Before this change, `platformbase.cpp` was only used by the `libslvs`
    build, and the code was copied from `platform.cpp`. Avoid the duplication
    by using `platformbase.cpp` in all build configurations and removing the
    duplicated code from `platform.cpp`

--- src/platform/platformbase.cpp
+++ src/platform/platformbase.cpp
@@ -3,6 +3,10 @@
 
 #include <mimalloc.h>
 
+#if defined(WIN32)
+#   include <windows.h>
+#endif // defined(WIN32)
+
 #include "util.h"
 #include "platform.h"
 
@@ -10,8 +14,48 @@ namespace SolveSpace {
 namespace Platform {
 
 //-----------------------------------------------------------------------------
-// Debug output
+// Debug output, on Windows.
 //-----------------------------------------------------------------------------
+
+#if defined(WIN32)
+
+#if !defined(_alloca)
+// Fix for compiling with MinGW.org GCC-6.3.0-1
+#define _alloca alloca
+#include <malloc.h>
+#endif
+
+void DebugPrint(const char *fmt, ...)
+{
+    va_list va;
+    va_start(va, fmt);
+    int len = _vscprintf(fmt, va) + 1;
+    va_end(va);
+
+    va_start(va, fmt);
+    char *buf = (char *)_alloca(len);
+    _vsnprintf(buf, len, fmt, va);
+    va_end(va);
+
+    // The native version of OutputDebugString, unlike most others,
+    // is OutputDebugStringA.
+    OutputDebugStringA(buf);
+    OutputDebugStringA("\n");
+
+#ifndef NDEBUG
+    // Duplicate to stderr in debug builds, but not in release; this is slow.
+    fputs(buf, stderr);
+    fputc('\n', stderr);
+#endif
+}
+
+#endif
+
+//-----------------------------------------------------------------------------
+// Debug output, on *nix.
+//-----------------------------------------------------------------------------
+
+#if !defined(WIN32)
 
 void DebugPrint(const char *fmt, ...) {
     va_list va;
@@ -20,6 +64,8 @@ void DebugPrint(const char *fmt, ...) {
     fputc('\n', stderr);
     va_end(va);
 }
+
+#endif
 
 //-----------------------------------------------------------------------------
 // Temporary arena.
